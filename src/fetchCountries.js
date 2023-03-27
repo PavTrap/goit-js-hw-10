535caf5a03528ae5f2b6f04709448f32d6aa12a7
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-03-27 18:32:35 +0300

    add function and descr

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
+const fields = 'fields=name,capital,population,flags,languages'
+
+// export function fetchCountries(name) {
+//   return fetch(`${BASE_URL}${name}?${fields}`)
+//     .then(response => response.json())
+//     .catch(error => console.log(error))
+// }
+export async function fetchCountries(name) {
+  try {
+    const response = await fetch(`${BASE_URL}${name}?${fields}`)
+    return await response.json()
+  } catch (error) {
+    return console.log(error)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,95 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const inputElem = document.querySelector('#search-box');
+const countryListElem = document.querySelector('.country-list');
+const countryInfoElem = document.querySelector('.country-info');
+
+const body = document.querySelector('body');
+body.style.display = 'flex';
+body.style.flexDirection = 'column';
+body.style.alignItems = 'center';
+body.style.textAlign = 'center';
+
+
+inputElem.addEventListener('input', debounce(handlerCountrySearch, DEBOUNCE_DELAY, { trailing: true }));
+
+
+  //------------------------------функція на введення із debounce------------------------------//
+  function handlerCountrySearch(e) {
+    //заборона перевантаження сторінки
+    e.preventDefault();
+
+    //------------------------------у місці введення беремо дані------------------------------//
+    const searchedCountry = inputElem.value.trim();
+
+    countryListElem.innerHTML = '';
+	 countryListElem.style.listStyle = 'none';
+
+    countryInfoElem.innerHTML = '';
+	 countryInfoElem.style.listStyle = 'none';
+
+    //------------------------------якщо порожня стрічка виходимо------------------------------//
+    if (!searchedCountry) {
+      countryListElem.innerHTML = '';
+      countryInfoElem.innerHTML = '';
+      return;
+    }
+
+    //------------------------------запуск функції із зовнішнього файлу, яка приймає введені дані------------------------------//
+    fetchCountries(searchedCountry)
+      .then(result => {
+      if (result.length > 10) {
+        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name');
+        return;
+      }
+      foundCountries(result);
+    })
+      .catch(error => {
+        countryListElem.innerHTML = '';
+        countryInfoElem.innerHTML = '';
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      })
+  };
+
+//------------------------------приймаємо результати------------------------------//
+function foundCountries(result) {
+  //------------------------------перевіряємо кількість знайдених даних------------------------------//
+  let inputData = result.length;
+
+  //------------------------------якщо знайдено від 2-10, ми виводимо просту розмітку------------------------------//
+  if (inputData >= 2 && inputData <= 10) {
+    const mark = result
+    .map(res => {
+      return `<li>
+      <img src="${res.flags.svg}" alt="Flag of ${res.name.official}" width="30" hight="20">
+        <p><b>${res.name.official}</b></p>
+      </li>`;
+    })
+    .join('');
+    countryListElem.innerHTML = mark;
+
+  //------------------------------якщо знайдено 1 результат, ми виводимо розширену розмітку------------------------------//
+        } else if (inputData === 1) {
+
+    const mark = result
+    .map(res => {
+      return `<li>
+      <img src="${res.flags.svg}" alt="Flag of ${res.name.official}" width="30" hight="20">
+        <p><b>${res.name.official}</b></p>
+        <p>Capital: ${res.capital}</p>
+        <p>Population: ${res.population}</p>
+        <p>Languages: ${Object.values(res.languages)} </p>
+      </li>`;
+    })
+    .join('');
+    countryListElem.innerHTML = mark;
+        }
+
+};
+
