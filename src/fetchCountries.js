1e3aec428a1cea1ae5e20b050a7a0f23c7f93c33
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-03-27 19:19:03 +0300

    qwe

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,20 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name/'
-const fields = 'fields=name,capital,population,flags,languages'
+// const BASE_URL = 'https://restcountries.com/v3.1/name/'
+// const fields = 'fields=name,capital,population,flags,languages'
 
-export function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.json())
-    .catch(error => console.log(error))
+// export function fetchCountries(name) {
+//   return fetch(`${BASE_URL}${name}?${fields}`)
+//     .then(response => response.json())
+//     .catch(error => console.log(error))
+// };
+export const fetchCountries = function (name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (response.status === 404) {
+        return[];
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
 };
